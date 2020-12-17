6085e5ade810350c1a25adaeabe822e41762cdcd
Daniel <danieruone@gmail.com>
2020-12-17 10:32:01 -0500

    scene

--- scripts/arHandler.js
+++ scripts/arHandler.js
@@ -79,26 +79,26 @@ function PrevModel() {
 }
 
 // wait until the model is loaded
-this.el.addEventListener("model-loaded", e => {
-  // grab the mesh 
+this.el.addEventListener("model-loaded", (e) => {
+  // grab the mesh
   let model = this.el.getObject3D("mesh");
-  // find the node with the basic material     
-  model.traverse(function(node) {
-    // ignore bones and other nodes without any material 
+  // find the node with the basic material
+  model.traverse(function (node) {
+    // ignore bones and other nodes without any material
     if (!node.material) return;
 
     // keep the reference to the old material - we want to dispose it later
-    var tmp = node.material
-    // substitute the material     
+    var tmp = node.material;
+    // substitute the material
     node.material = new THREE.MeshStandardMaterial({
-       skinning: true, // the original material is using skinning
-       map: node.material.map // we want the original texture
+      skinning: true, // the original material is using skinning
+      map: node.material.map, // we want the original texture
     });
     // update and clean up
     node.material.needsUpdate = true;
-    tmp.dispose()
-  }
-})
+    tmp.dispose();
+  });
+});
 
 function analyticsEvent(evento, tiempo) {
   gtag("event", "click", {

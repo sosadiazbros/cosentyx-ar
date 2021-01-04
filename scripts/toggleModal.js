77b5f8619daf9bc47d7838a8e78234d700e01126
Daniel <danieruone@gmail.com>
2021-01-04 09:34:29 -0500

    new models and steps modal

--- scripts/toggleModal.js
+++ scripts/toggleModal.js
@@ -1,9 +1,18 @@
 let modal = document.getElementById("footerModal");
+let modal2 = document.getElementById("footerModal2");
 
 function toggleModal() {
   if (modal.classList.contains("hideModal")) {
     modal.classList.remove("hideModal");
   } else {
     modal.classList.add("hideModal");
+  }
+}
+
+function toggleModal2() {
+  if (modal2.classList.contains("hideModal")) {
+    modal2.classList.remove("hideModal");
+  } else {
+    modal2.classList.add("hideModal");
   }
 }
